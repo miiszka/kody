513bd0658555ec021647dc91d116e88c0b48e107
miiszka <emeryt555@tlen.pl>
2017-10-13 11:39:38 +0200

    pętle switch

---
+++ cpp/petle_znak.cpp
@@ -0,0 +1,25 @@
+/*
+ * petle_switch.cpp
+ * Program pobiera numer miesiąca i wyswietla jego nazwę
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    char zn='t';
+   
+    while ( = ) {
+    
+        cout << "Podaj miesiąc (1-12): " << endl;
+        cin >> m;
+    };
+
+   
+    
+    return 0;
+}
+
