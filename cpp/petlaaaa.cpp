150f0603c1023da659296afb926ae93a5ee98bfe
miiszka <emeryt555@tlen.pl>
2017-10-17 18:45:35 +0200

    praca dom

---
+++ cpp/petlaaaa.cpp
@@ -0,0 +1,28 @@
+/*
+ * petlaaaa.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int x = 0;
+    
+    while (x > 0 || x <= 15)
+    {
+        cout << "Podaj liczbę: " << endl;
+        cin >> x;
+        cout << "x = " << x << endl;
+        
+    }
+    
+    
+    
+    
+	
+	return 0;
+}
+
