a2a9dcd58e1e536978322af5ef5979eb21407dfb
miiszka <emeryt555@tlen.pl>
2017-10-24 13:30:03 +0200

    tablice

---
+++ cpp/tablice.cpp
@@ -0,0 +1,35 @@
+/*
+ * tablice.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char **argv)
+{
+    int liczby[5];
+    int i;
+    int suma = 0;
+    
+    cout << "Podaj 5 ocen (0-6): " << endl;
+    for (i = 0; i < 5; i++){
+        cin >> liczby[i];
+    }
+    cout << "Podane oceny: " << endl;
+    
+    for (i = 0; i < 5; i++){
+        suma += liczby [i];
+        cout << liczby[i] << " ";
+        
+    }
+    cout << "Suma ocen: " << suma << endl;
+    cout << "Średnia arytmetyczna: " << float(suma)/float(5) << endl;
+    
+    
+    
+    return 0;
+}
+
---
+++ cpp/tablice2.cpp
@@ -0,0 +1,39 @@
+/*
+ * tablice.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char **argv)
+{
+    //const int rozmiar = 5;
+    int rozmiar = 0;
+    cout << "Ile ocen: " << endl;
+    cin >> rozmiar;
+    int liczby[rozmiar];
+    int i;
+    int suma = 0;
+    
+    cout << "Podaj 5 ocen (0-6): " << endl;
+    for (i = 0; i < rozmiar; i++){
+        cin >> liczby[i];
+    }
+    cout << "Podane oceny: " << endl;
+    
+    for (i = 0; i < rozmiar; i++){
+        suma += liczby [i];
+        cout << liczby[i] << " ";
+        
+    }
+    cout << "Suma ocen: " << suma << endl;
+    cout << "Średnia arytmetyczna: " << float(suma)/float(rozmiar) << endl;
+    
+    
+    
+    return 0;
+}
+
