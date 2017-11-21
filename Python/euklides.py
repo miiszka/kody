afb3985a0fa4244aff026a7a0e1d3859ab6011d7
miiszka <emeryt555@tlen.pl>
2017-11-21 13:31:58 +0100

    euklides

---
+++ Python/euklides.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def euklides(a, b):
+    while a != b:
+        if a > b:
+            a = a - b
+        else:
+            b = b - a
+    return a
+
+def euklides2(a, b):
+    while a > 0:
+        a = a % b
+        b = b - a
+
+    return b
+
+
+def main(args):
+
+    a = int(input("Podaj liczbę: "))
+    b = int(input("Podaj liczbę: "))
+    assert euklides2(1989, 867) == 51
+    print("Nwd({:d}, {:d}) = {:d}".format(a, b, euklides2(a, b)))
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
