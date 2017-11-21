7095c9ccda9aca7e50b1fd19d9c07e03192e10af
miiszka <emeryt555@tlen.pl>
2017-11-21 06:11:34 +0100

    screen

--- Python/witaj.py
+++ Python/witaj.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# osoba = "werter"
-# osoba = 'werter'
-osoba = input('Jak się nazywasz??????\n')
+# osoba = "Michalina"
+# osoba = 'Michalina'
+osoba = input('Jak się nazywasz?\n')
 print("Witaj", osoba, "!")
 
 rok = 2017
-wiek = int(input('Ile masz latek?\n'))
+wiek = int(input('Ile masz lat?\n'))
 print('Urodziłeś się w', rok - wiek, "roku!")
