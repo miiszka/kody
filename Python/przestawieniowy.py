d08a1a62504a849248f83b1b91ee682b3d3a37e4
miiszka <emeryt555@tlen.pl>
2018-02-07 12:36:55 +0100

    szyfrowanie

---
+++ Python/przestawieniowy.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def szyfruj(tekst, klucz):
+    reszta = len(tekst) % klucz
+    if reszta:
+        tekst += (klucz - reszta) * "."
+        szyfrogram = ""
+    for i in range(klucz):
+        for j in range(int(len(tekst) / klucz)):
+            # 0, 10, 20, 30
+            # 1, 11, 21, 31
+            print(i + j * klucz, tekst[i + j * klucz])
+    return szyfrogram
+
+
+def deszyfruj():
+    pass
+
+
+def main(args):
+    tekst = input("Podaj tekst: ")
+    klucz = int(input("Podaj klucz: "))
+    while (2 * klucz > len(tekst)):
+        klucz = int(input("Podaj klucz: "))
+    szyfrogram = szyfruj(tekst, klucz)
+    print(szyfrogram)
+    # print(deszyfruj(szyfrogram, klucz))
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
