c1d66bd687c34604191844b56984e11dede86a21
miiszka <emeryt555@tlen.pl>
2018-02-27 13:31:18 +0100

    a<yfrowanie

--- Python/szyfr_ceazra.py
+++ Python/szyfr_ceazra.py
@@ -6,12 +6,12 @@ def deszyfruj(szyfrogram, klucz):
 	tekst = ""
     klucz = klucz % 26
     for znak in szyfrogram:
-        if ord(znak)>64 and ord(znak)<91:
-            ascii = ord(znak) - klucz
-            if ascii > 90:
-                ascii -= 26
-        if ord(znak)>96 and ord(znak)<123:
-            ascii = ord(znak) - klucz
+        if ord(znak) > 64 and ord(znak) < 91:
+        ascii = ord(znak) - klucz
+        if ascii > 90:
+            ascii -= 26
+            if ord(znak)>96 and ord(znak)<123:
+                ascii = ord(znak) - klucz
         tekst += chr(ascii)
     return tekst
 
