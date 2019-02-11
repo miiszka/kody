309d8adf6cf1e8dc534fdbe82ceb8eab041111f8
miiszka <emeryt555@tlen.pl>
2019-02-11 13:01:45 +0100

    akt

--- bazy/uczniowie/kwerendy_orm.py
+++ bazy/uczniowie/kwerendy_orm.py
@@ -1,26 +1,111 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  klasa_uczen.py
+#  kwerendy_orm.py
 import os
 from uczniowie_model import *
 
+def kw08():
+    """ lista nazwisk uczniów i nazwy klas """
+    query = (Uczen
+        .select(Uczen.nazwisko, Uczen.klasa.klasa)
+        .join(Klasa)
+    )
 
+    for obj in query:
+        print(obj.nazwisko, obj.klasa.klasa)
 
-def main(args):
-    baza.connect() # połączenie z bazą 
+def kw09():
+    """ lista klas i ilość  uczniów """
+    query = (Uczen
+        .select(fn.Count(Uczen.id).alias('ilu'), Uczen.klasa.klasa)
+        .join(Klasa)
+        .group_by(Klasa)
+        .order_by(SQL('ilu').asc())
+    )
+
+    for obj in query:
+        print(obj.klasa.klasa, obj.ilu)
+
+def kw10():
+    """ lista przedmiotów i ilość ocen """
+    query = (Ocena
+        .select(fn.Count(Ocena.id).alias('ile'), Ocena.przedmiot.przedmiot)
+        .join(Przedmiot)
+        .group_by(Przedmiot)
+        .order_by(SQL('ile').asc())
+    )
+
+    for obj in query:
+        print(obj.przedmiot.przedmiot, obj.ile)
+
+def kw11():
+    """ Po ile ocen mają uczniowie """
+    query = (Ocena
+        .select(Ocena.uczen.nazwisko, fn.Count(Ocena.id).alias('ile'))
+        .join(Uczen)
+        .group_by(Ocena.uczen.nazwisko)
+        .order_by(SQL('ile').asc())
+    )
+    for obj in query:
+        print(obj.uczen.nazwisko, obj.ile)
+        
+def kw12():
+    """ średnie ocen poszczególnych uczniów """
+    query = (Ocena
+        .select(Ocena.uczen.nazwisko, fn.AVG(Ocena.ocena).alias('sr'))
+        .join(Uczen)
+        .group_by(Ocena.uczen.nazwisko)
+        .order_by(AVG('sr').asc())
+    )
+    for obj in query:
+        print(obj.uczen.nazwisko, round(obj.srednia, 2))
+        
+def kw13():
+    """ średnie ocen ucznia Szymczak z poszczególnych przedmiotów """
+        .select(Ocena.uczen.nazwisko, Ocena.przedmiot.przedmiot, fn.AVG(Ocena.ocena).alias('sr'))
+        .join(Uczen)
+        .join_from(Ocena, Przedmiot)
+        .where(Ocena.uczen.nazwisko == 'Szymczak')
+        .group_by(Ocena.przedmiot.przedmiot)
+        .order_by(AVG('sr').asc())
+    )
+    for obj in query:
+        print(obj.uczen.nazwisko, obj.przedmiot.przedmiot, round(obj.srednia, 2))
     
+def kw14():
+    """ilu uczniów ma średnią powyżej 3.5 z WF? """
+    Srednie = (Ocena
+        .select(Ocena.uczen.nazwisko, fn.AVG(Ocena.ocena).alias('srednia'))
+        .join(Uczen)
+        .join_from(Ocena, Przedmiot)
+        .where(Ocena.przedmiot.przedmiot == 'WF')
+        .group_by(Ocena.uczen.nazwisko)
+    )
+    for obj in query:
+        if (obj.srednia > 3.5):
+            print(obj.uczen.nazwisko, round(obj.srednia, 2))
+    
+    
+    
+    
+    
+def main(args):
+    baza.connect()  # połączenie z bazą
+
     kwerendy = [
-    "Uczen.select()"
-    "Uczen.select().where(Uczen.imie=='Rafał')",
-    "Uczen.select().where(Uczen.egz_mat.between(20, 40))",
-    "Uczen.select().where((Uczen.nazwisko=='Piasecki') & (Uczen.imie=='Rafał'))",
-    "Uczen.select().order_by(Uczen.egz_mat.desc())"
-    "Uczen.select().where(Uczen.plec==1).order_by(Uczen.egz_mat.desc())"
+        "Uczen.select()",
+        "Uczen.select().where(Uczen.imie=='Rafał')",
+        "Uczen.select().where(Uczen.imie.startswith('G'))",
+        "Uczen.select().where(Uczen.egz_mat.between(20, 40))",
+        "Uczen.select().where((Uczen.nazwisko=='Piasecki') & (Uczen.imie=='Rafał'))",
+        "Uczen.select().order_by(Uczen.egz_mat.asc())",
+        "Uczen.select().where(Uczen.plec==1).order_by(Uczen.egz_mat.asc())",
     ]
     
-    for obj in eval(kwerendy[4]):
-        print(obj.nazwisko, obj.imie, obj.egz_mat)
+    #for obj in eval(kwerendy[6]):
+    #    print(obj.nazwisko, obj.imie, obj.egz_mat)
+    kw10()
     
     baza.commit()
     baza.close()
--- bazy/uczniowie/uczniowie_model.py
+++ bazy/uczniowie/uczniowie_model.py
@@ -1,45 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-
 from peewee import *
 
 
 baza_plik = 'test_orm.db'
 baza = SqliteDatabase(baza_plik)  # instancja wykorzystywanej bazy
 
-######## MODELE #######
+########## MODELE #####
 class BazaModel(Model):
     class Meta:
         database = baza
-    
-    
+
 
 class Klasa(BazaModel):
     klasa = CharField(null=False)
     rok_naboru = IntegerField(default=0)
     rok_matury = IntegerField(default=0)
 
-
 class Uczen(BazaModel):
     imie = CharField(null=False)
     nazwisko = CharField(null=False)
-    plec = BooleanField()
+    plec = IntegerField()
     klasa = ForeignKeyField(Klasa, related_name='uczniowie')
-    egzhum = FloatField(default=0)
-    egzmat = FloatField(default=0)
-    egzjez = FloatField(default=0)
-
+    egz_hum = DecimalField(default=0)
+    egz_mat = DecimalField(default=0)
+    egz_jez = DecimalField(default=0)
 
 class Przedmiot(BazaModel):
     przedmiot = CharField(null=False)
-    imienaucz = CharField(null=False)
-    nazwiskonaucz = CharField(null=False)
-    plecnaucz = BooleanField()
-
-
+    imie_naucz = CharField(null=False)
+    nazwisko_naucz = CharField(null=False)
+    plec_naucz = IntegerField()
+    
 class Ocena(BazaModel):
-    datad = DateField(null = False) 
+    datad = DateField()
     uczen = ForeignKeyField(Uczen, related_name='oceny')
     przedmiot = ForeignKeyField(Przedmiot, related_name='oceny')
-    ocena = FloatField(default=0)
+    ocena = DecimalField(null=False)
